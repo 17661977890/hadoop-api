dff7ddfc54208800b01df9c3308a1d95b6cf1763
彬 <17661977890@163.com>
2019-07-17 16:07:25 +0800

    hadoop 的java相关文件操作api

---
+++ src/main/java/com/example/hadoopapi/HadoopApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hadoopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadoopApiApplication.class, args);
+	}
+
+}
